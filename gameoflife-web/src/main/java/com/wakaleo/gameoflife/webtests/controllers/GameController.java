8c8b56f1c125bbf5984aebd52852b781aff72c37
Naren V <aaku481@yahoo.com>
2017-07-30 09:35:03 -0500

    from regular desktop

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -115,7 +115,7 @@ public class GameController {
 	// Getter for this class' random number generator
 	// Good practice to avoid directly accessing attributes, use getters and setters instead
     private Random getRandomGenerator() {
-	System.out.println("Hello Nareddar");
+    	System.out.println("Hello Nareddar Garu");
         return randomGenerator;
     }
 }
